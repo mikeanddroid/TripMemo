b4dfa2e6b0845934cd20351442af8a09113444d5
GiveMeWingzz <michael.henry.tirkey@gmail.com>
2017-03-16 15:28:51 -0400

    Initial Commit 2

--- app/src/main/java/com/mike/givemewingzz/tripmemo/MainActivity.java
+++ app/src/main/java/com/mike/givemewingzz/tripmemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String initialCommitTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
