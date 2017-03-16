060e7fb805d0204d19e782b991f126daf7050eba
GiveMeWingzz <michael.henry.tirkey@gmail.com>
2017-03-16 15:29:44 -0400

    Deleting initial commit

--- app/src/main/java/com/mike/givemewingzz/tripmemo/MainActivity.java
+++ app/src/main/java/com/mike/givemewingzz/tripmemo/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    String initialCommitTest;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
